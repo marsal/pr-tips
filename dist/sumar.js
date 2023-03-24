7d79968b6f848ee4c86a2aaa32b3c8f2d2faf7ac
marsal.sans <marsal.sans@gmail.com>
2023-03-24 09:14:41 +0100

    test github actions (#2)

---
+++ dist/sumar.js
@@ -0,0 +1,12 @@
+"use strict";
+// sumar.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sumar = void 0;
+function sumar(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+        return NaN;
+    }
+    return a + b;
+}
+exports.sumar = sumar;
+//# sourceMappingURL=sumar.js.map
